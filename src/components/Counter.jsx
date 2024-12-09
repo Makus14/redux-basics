06997cd759f516d5779f57735a656132a8855bbd
Makus14 <mesha.maksim14@gmail.com>
2024-12-09 16:58:23 +0300

    dispatching actions

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,29 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+
+  const incrementHandle = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHandle = () => {
+    dispatch({ type: "decrement" });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandle}>Increment</button>
+        <button onClick={decrementHandle}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
 import "./index.css";
 import store from "./store/index.js";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
